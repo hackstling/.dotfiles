35c1a91e43bd47614cc3d457df5f3f2ab90a841d
punisher <norbert@punisher>
2025-07-24 13:56:18 +0200

    Auto-sync from Kali GNU/Linux: Thu Jul 24 01:56:18 PM CEST 2025

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,8 +1,8 @@
 return {
     "catppuccin/nvim",
-    lazy = false,
+    lazy = true,
     name = "catppuccin",
-    priority = 1000,
+    -- priority = 1000,
     config = function()
         vim.cmd.colorscheme "catppuccin"
     end
---
+++ nvim/.config/nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,20 @@
+return {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false,  -- Load on startup
+    priority = 1000,
+    config = function()
+        require("gruvbox").setup({
+            contrast = "hard",         -- or "soft", or "" for default
+            italic = {
+                strings = false,
+                comments = true,
+                operators = false,
+                folds = true,
+            },
+            overrides = {},
+        })
+
+        vim.o.background = "dark"       -- or "light" for gruvbox light mode
+        vim.cmd.colorscheme("gruvbox")  -- Set the actual colorscheme
+    end,
+}
