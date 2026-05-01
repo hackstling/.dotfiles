4d09580d8c7e63466c8d8c379d5a7d468de4c5e5
punisher <norbert@punisher>
2026-05-01 14:19:21 +0200

    Auto-sync from Kali GNU/Linux: Fri May  1 02:19:21 PM CEST 2026

---
+++ nvim/.config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,5 @@
+return {
+  "tpope/vim-surround",
+  event = "VeryLazy",        -- loads after startup (recommended)
+  dependencies = { "tpope/vim-repeat" },   -- highly recommended for better . repeating
+}
